e8a6483074f6182c77e2b28b4a9767870f78d041
Tommy Pyatt <tommypyatt@users.noreply.github.com>
2022-05-31 10:53:11 +0100

    Group sudo commands together

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y curl vim
 
 # Add third party package repos
-sudo add-apt-repository ppa:ondrej/php
+sudo add-apt-repository -y ppa:ondrej/php
 curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list
 sudo apt update
@@ -13,9 +12,37 @@ sudo apt install -y elasticsearch
 # Install applications
 sudo apt install -y nginx curl mariadb-server php7.4-{bcmath,common,curl,fpm,gd,intl,mbstring,mysql,soap,xml,xsl,zip,cli,xml,dev,xdebug}
 
+# Clean up
+sudo apt autoremove -y
+
+# Add an nginx config for magento.test
+echo -e "server {\n\
+    listen 80;\n\
+    server_name magento.test;\n\
+    set \$MAGE_ROOT /home/$USER/repos/magento;\n\
+    include /home/$USER/repos/magento/nginx.conf;\n\
+}" | sudo tee /etc/nginx/sites-available/magento.conf
+sudo ln -s /etc/nginx/sites-available/magento.conf /etc/nginx/sites-enabled/magento.conf
+
+# Add upstream fastcgi backend for PHP 7.4
+echo -e "upstream fastcgi_backend74 {\n\
+    server   unix:/var/run/php/php7.4-fpm.sock;\n\
+}" | sudo tee /etc/nginx/conf.d/fastcgi_php74.conf
+
+# Add a hosts file entry
+echo -e "127.0.1.1       magento.test" | sudo tee -a /etc/hosts
+
+# Assign a big enough heap size to elasticsearch for Magento
+echo -e "-Xms256m\n-Xmx256m" | sudo tee /etc/elasticsearch/jvm.options.d/heap.options
+
 # Start elasticsearch and enable start on boot
 sudo service elasticsearch start
 sudo systemctl enable elasticsearch.service
+
+# Create a DB user and database
+sudo mysql -e "create user 'magento'@'localhost' identified by 'magento'"
+sudo mysql -e "grant all privileges on *.* to 'magento'@'localhost'"
+mysql -umagento -pmagento -e "create database magento"
 
 # Set up groups for permissions
 sudo usermod -a -G www-data $USER
@@ -44,29 +71,7 @@ cp nginx.conf.sample nginx.conf
 sed -i "s/fastcgi_backend/fastcgi_backend74/g" ./nginx.conf
 echo '/bin/php7.4' > .php-version
 
-# Create a DB user and database
-sudo mysql -e "create user 'magento'@'localhost' identified by 'magento'"
-sudo mysql -e "grant all privileges on *.* to 'magento'@'localhost'"
-mysql -umagento -pmagento -e "create database magento"
-
-# Install the M-dog
+# Install Magento
 /bin/php7.4 bin/magento setup:install --base-url=http://magento.test --db-host=localhost --db-name=magento --db-user=magento --db-password=magento --admin-firstname=admin --admin-lastname=admin --admin-email=admin@admin.com --admin-user=admin --admin-password=admin123 --language=en_GB --currency=GBP --timezone=Europe/London --use-rewrites=1 --search-engine=elasticsearch7 --elasticsearch-host=localhost --elasticsearch-port=9200 --elasticsearch-index-prefix=magento2 --elasticsearch-timeout=15 --backend-frontname=admin
 /bin/php7.4 bin/magento deploy:mode:set developer
 /bin/php7.4 bin/magento module:disable Magento_TwoFactorAuth
-
-# Add an nginx config for magento.test
-echo -e "server {\n\
-    listen 80;\n\
-    server_name magento.test;\n\
-    set \$MAGE_ROOT /home/$USER/repos/magento;\n\
-    include /home/$USER/repos/magento/nginx.conf;\n\
-}" | sudo tee /etc/nginx/sites-available/magento.conf
-sudo ln -s /etc/nginx/sites-available/magento.conf /etc/nginx/sites-enabled/magento.conf
-
-# Add upstream fastcgi backend for PHP 7.4
-echo -e "upstream fastcgi_backend74 {\n\
-    server   unix:/var/run/php/php7.4-fpm.sock;\n\
-}" | sudo tee /etc/nginx/conf.d/fastcgi_php74.conf
-
-# Add a hosts file entry
-echo -e "127.0.1.1       magento.test" | sudo tee -a /etc/hosts
